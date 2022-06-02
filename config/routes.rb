b1da3adafd05db53365f20eaa11097dc1be7a92d
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-06-03 01:04:31 +0300

    [tela] Add dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "dashboards#index"
 end
